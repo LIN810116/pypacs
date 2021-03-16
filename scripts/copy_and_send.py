7adbb24434dca0889bb750d0ec8e68219d12c9a4
Chinchien <LIN810116@gmail.com>
2021-03-16 16:04:08 +1300

    Update example scripts

--- scripts/copy_and_send.py
+++ scripts/copy_and_send.py
@@ -1,3 +1,18 @@
+"""
+Retrieve data from the pacs of your choose (sender) and send to another pacs (receiver).
+The receiver pacs needs to be configured properly in the sender pacs on the AE management page/configuration file.
+
+Also. make sure both the sender and receiver nodes are running before sending files.
+Below are the steps to set up a receiver node using storescp:
+    Prerequisites: you need have dcmtk package installed:
+        - sudo apt-get update
+        - sudo apt-get install -y dcmtk
+    1. Create a folder
+    2. Open a terminal from the folder
+    3. Run:
+        sudo storescp -v <portNumber> -tos 3 -dhl -ss dcm -tn
+"""
+
 import json
 from src.pypacs import move_files
 
@@ -7,20 +22,17 @@ if __name__ == '__main__':
 
     with open(conf_path) as config_file:
         cfg = json.load(config_file)
-    server_ip = cfg['server_ip']
-    server_port = cfg['server_port']
-    aec = cfg['aec']
-    aet = cfg['aet']
+    server_ip = cfg.get('server_ip')
+    server_port = cfg.get('server_port')
+    aec = cfg.get('aec')
+    aet = cfg.get('aet')
 
-    # Retrieve data from the pacs of your choose (sender) and send to another pacs (receiver).
-    # The receiver pacs needs to be configured appropriately in the sender pacs in the AE management page/configuration file.
-    # Also. make sure both the sender and receiver are running before sending files.
-    # TODO. write your query here.
     # Currently, you have to provide StudyInstanceUID and SeriesInstanceUID.
-    # Future improvement: allow user to send data from patient, study, series or image level.
+    # Future improvement: allow user to send data from patient, study, series or image levels.
+    # TODO. write your query here.
     query_settings = {
-        'StudyInstanceUID': '1.3.12.2.1107.5.2.30.25138.30000006060820152293700000008',
-        'SeriesInstanceUID': '1.3.12.2.1107.5.2.30.25138.30000006060820125681200002569'
+        'StudyInstanceUID': '1.3.12.2.1107.5.2.30.25138.30000006060122155281200000007',
+        'SeriesInstanceUID': '1.3.12.2.1107.5.2.30.25138.30000006060120104693700002235'
     }
 
     move_files(server_ip=server_ip, server_port=server_port, aec=aec, aet=aet, query_settings=query_settings)
--- scripts/query_and_save_metadata.py
+++ scripts/query_and_save_metadata.py
@@ -1,5 +1,5 @@
 import json
-from src.pypacs import get_metadata, create_custom_report, save_metadata
+from src.pypacs import get_metadata, create_custom_report, save_metadata, filter_by_extra_query
 
 if __name__ == '__main__':
     # TODO: get the config file of the pacs you want to access
@@ -7,21 +7,42 @@ if __name__ == '__main__':
 
     with open(conf_path) as config_file:
         cfg = json.load(config_file)
-    server_ip = cfg['server_ip']
-    server_port = cfg['server_port']
-    aec = cfg['aec']
+    server_ip = cfg.get('server_ip')
+    server_port = cfg.get('server_port')
+    aec = cfg.get('aec')
 
     # TODO. write your query here
     query_settings = {
         'PatientID': 'VL*'
     }
+    # query_settings = {
+    #     'PatientID': 'VL00001',
+    #     'Modality': 'MR'
+    # }
+    # query_settings = {
+    #     'PatientID': 'CL*',
+    #     'Modality': 'MR',
+    # }
 
     # get metadata
     metadata = get_metadata(server_ip, server_port, aec, query_settings)
+
+    # # TODO. filter by extra query
+    extra_query = [
+        # NumberOfSeriesRelatedInstances
+        {
+            'tag': 'NumberOfSeriesRelatedInstances',
+            'operator': '>',
+            'value': 100
+        }
+    ]
+
+    metadata = filter_by_extra_query(metadata, extra_query)
+
     report = create_custom_report(metadata)
-    print(report)
+    # print(report)
     # save custom report
-    save_metadata(report, "../out/custom_report.json")
+    save_metadata(report, "../out/custom_report_test.json")
     # save metadata in the default format
     # save_metadata(metadata, "../out/metadata.json")
     # save_metadata(metadata['report']['json'], "../out/report.json")
